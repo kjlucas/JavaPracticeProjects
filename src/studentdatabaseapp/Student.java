d1da2c32c0957465b57709e47d28c2c42e2fe691
Kamau Lucas <kjlucas6@gmail.com>
2020-07-01 09:38:17 -0500

    completed

--- src/studentdatabaseapp/Student.java
+++ src/studentdatabaseapp/Student.java
@@ -1,5 +1,96 @@
 package studentdatabaseapp;
 
+import java.util.Scanner;
+
 public class Student {
+	private String firstName, lastName, courses, studentId;
+	private int tuitionBalance, year;
+	private final static int courseCost = 600;
+	private static int[] id = { 0, 0, 0, 0 };
+
+	Student() {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter student's first name: ");
+		firstName = in.nextLine();
+
+		System.out.print("Enter student's last name: ");
+		lastName = in.nextLine();
+
+		System.out.print("1 - Freshman\n2 - Sophmore\n3 - Junior\n4 - Senior\nEnter number correspoding to grade:");
+		year = in.nextInt();
+		setStudentId();
+
+		System.out.println(firstName + " " + lastName + ", year " + year + "\nID: " + studentId);
+	}
+
+	public void enroll() {
+
+		Scanner in = new Scanner(System.in);
+		do {
+			System.out.println("Enter course to enroll (Q to quit): ");
+			String course = in.nextLine();
+			if (!course.equalsIgnoreCase("q")) {
+
+				if (courses == null) {
+					courses = course;
+				} else {
+					courses = courses + "\n" + course;
+				}
+
+				tuitionBalance += courseCost;
+			} else {
+				break;
+			}
+
+		} while (true);
+		System.out.println("Enrolled in:\n" + courses);
+		viewTuitionBalance();
+	}
+
+	public String getCourses() {
+		return courses;
+	}
+
+	public void viewTuitionBalance() {
+		System.out.println("Current Balance is $" + tuitionBalance);
+	}
+
+	public void payTuition() {
+		viewTuitionBalance();
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter payment amount: ");
+		int payment = in.nextInt();
+		tuitionBalance -= payment;
+
+		if (tuitionBalance > 0) {
+			System.out.println("Your payment of $" + payment + " has been proccesed.");
+			viewTuitionBalance();
+		} else if (tuitionBalance == 0) {
+			System.out.println("Your payment of $" + payment + " has been proccesed.");
+			System.out.println("\nYou have paid off entire balance. Enjoy your semester!");
+			viewTuitionBalance();
+		} else {
+			System.out.println("Your payment of $" + payment + " has been proccesed.");
+			System.out.println("\nYou have paid off entire balance. Enjoy your semester!");
+			System.out.println("Refund Avaliable : $" + Math.abs(tuitionBalance));
+			tuitionBalance = 0;
+		}
+
+	}
+	
+	public String toString() {
+		return "Name: " + firstName + " " + lastName + "\nYear " + year +  
+				"\nStudent ID: " + studentId +"\nEnrolled Courses:\n" + courses + "\nTuition Balance: $"+
+				tuitionBalance;
+	}
+
+	public String getStudentId() {
+		return studentId;
+	}
+
+	private void setStudentId() {
+		id[year]++;
+		this.studentId = ((year * 1000) + id[year]) + "";
+	}
 
 }
--- src/studentdatabaseapp/StudentDatabaseApp.java
+++ src/studentdatabaseapp/StudentDatabaseApp.java
@@ -1,10 +1,23 @@
 package studentdatabaseapp;
 
+import java.util.Scanner;
+
 public class StudentDatabaseApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner in = new Scanner(System.in);
+		System.out.print("How many student would you like to enroll?");
+		Student[] students = new Student[in.nextInt()];
+		
+		for(int i = 0; i < students.length; i++) {
+			students[i] = new Student();
+			students[i].enroll();
+			students[i].payTuition();
+		}
+		for(Student i : students) {
+			System.out.println(i.toString());
+		}
 	}
 
 }
